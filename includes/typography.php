ca5451790a43bdb5653fc8e385100d09f6c7ed99
Jabal Torres <jabaltorres@gmail.com>
2015-03-02 17:22:19 +0300

    minor updates and changes

---
+++ includes/typography.php
@@ -0,0 +1,37 @@
+<div class="row">
+  <div class="large-12 columns content arabic">
+    <!-- <h2>Al Jazeera Arabic</h2> -->
+    <div class="large-9 large-offset-1 columns">
+      <h1>شبكة الجزيرة الإعلامي.</h1>
+      <span>This will be the details</span>
+      <h2>شبكة الجزيرة الإعلامي.</h2>
+      <span>This will be the details</span>
+      <h3>شبكة الجزيرة الإعلامي.</h3>
+      <span>This will be the details</span>
+      <h4>شبكة الجزيرة الإعلامي.</h4>
+      <span>This will be the details</span>
+      <h5>شبكة الجزيرة الإعلامي.</h5>
+      <span>This will be the details</span>
+      <h6>شبكة الجزيرة الإعلامي.</h6>
+      <span>This will be the details</span>
+    </div>
+  </div>
+
+  <div class="large-12 columns content helvetica">
+    <!-- <h2>Helvetica Neue</h2> -->
+    <div class="large-9 large-offset-1 columns">
+      <h1>This is a very large header.</h1>
+      <span>This will be the details</span>
+      <h2>This is a large header.</h2>
+      <span>This will be the details</span>
+      <h3>This is a medium header.</h3>
+      <span>This will be the details</span>
+      <h4>This is a moderate header.</h4>
+      <span>This will be the details</span>
+      <h5>This is a small header.</h5>
+      <span>This will be the details</span>
+      <h6>This is a tiny header.</h6> 
+      <span>This will be the details</span>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,44 +1,17 @@
-<?php include 'header.php';?>
+<?php include 'head.php';?>
 
 <span id="style" class="divider"></span>
-<?php include 'components/hero.php';?>  
+<?php include 'includes/hero.php';?>  
 
 
 <span id="typography" class="divider"></span>
 <div class="row">
   <div class="large-12 columns">
     <h1>Typography</h1>
-    
-    <!-- This is the beginning of the playground-->
-
-    <h2>This is cool</h2>
-    <blockquote>
-      <p>Design is the method of putting form and content together. Design, just as art, has multiple definitions; there is no single definition. Design can be art. Design can be aesthetics. Design is so simple, that's why it is so complicated.</p>
-      <cite><a href="#">Paul Rand</a></cite>
-    </blockquote>
-
-    <p>Luke continued, <q>And then she called him a <q>scruffy-looking nerf-herder</q>! I think I’ve got a chance!</q> The poor naive fool…</p>
-
-
-    <!-- This is the end of the playground-->
-
   </div>
   <hr>
 </div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/typography.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <script src="https://gist.github.com/capitalJT/98915dd22812dea58db7.js"></script>
-    </div>
-</div>
+<?php include 'includes/typography.php';?>
 
 <span id="brand-colors" class="divider"></span>
 <div class="row">
@@ -47,21 +20,7 @@
   </div>
   <hr>
 </div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/colors.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <script src="https://gist.github.com/capitalJT/844d41b4f8d1c7e7ad87.js"></script>
-    </div>
-</div>
-
+<?php include 'includes/colors.php';?>
 
 <span id="imagery" class="divider"></span>
 <div class="row">
@@ -70,44 +29,16 @@
   </div>
   <hr>
 </div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/imagery.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <script src="https://gist.github.com/capitalJT/844d41b4f8d1c7e7ad87.js"></script>
-    </div>
-</div>
-
+<?php include 'includes/imagery.php';?>
 
 <span id="buttons" class="divider"></span>
 <div class="row">
   <div class="large-12 columns">
-      <h1>Buttons</h1>
-    </div>
+    <h1>Buttons</h1>
   </div>
+  <hr>
 </div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/buttons.php';?>
-<div class="row code">
-  <div class="large-12 columns content">
-    <h5>HTML &amp; CSS</h5>
-  </div>
-  <div class="large-12 columns">
-    <script src="https://gist.github.com/capitalJT/2790f39facc8b40a8259.js"></script>
-  </div>   
-</div>
-
+<?php include 'includes/buttons.php';?>
 
 <span id="lists" class="divider"></span>
 <div class="row">
@@ -116,122 +47,6 @@
   </div>
   <hr>
 </div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/lists.php';?>
-<div class="row code">
-  <div class="large-12 columns content">
-    <h5>HTML &amp; CSS</h5>
-  </div>
-  <div class="large-12 columns">
-    <script src="https://gist.github.com/capitalJT/588ed43de659428dee15.js"></script>
-  </div>   
-</div>
-
-
-<span id="components" class="divider"></span>
-<div class="row">
-  <div class="large-12 columns hero">
-    <h1>COMPONENTS</h1>
-  </div>
-</div>
-
-
-<span id="headers" class="divider"></span>
-<div class="row">
-  <div class="large-12 columns">
-    <h1>Header-1</h1>
-  </div>
-  <hr>
-</div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/header-1.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <script src="https://gist.github.com/capitalJT/64e2cfc06de3d7245072.js"></script>
-    </div>
-</div>
-
-
-<span id="masthead" class="divider"></span>
-<div class="row">
-  <div class="large-12 columns">
-    <h1>Masthead</h1>
-  </div>
-  <hr>
-</div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/masthead.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <script src="https://gist.github.com/capitalJT/548ca6c30f564ae219f9.js"></script>
-    </div>
-</div>
-
-
-<span id="carousel" class="divider"></span>
-<div class="row">
-  <div class="large-12 columns">
-    <h1>Full-Width Carousel</h1>
-  </div>
-  <hr>
-</div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/carousel.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <script src="https://gist.github.com/capitalJT/05fd31882a9e257eb83a.js"></script>
-    </div>
-</div>
-
-
-<span id="body-content" class="divider"></span>
-<div class="row">
-  <div class="large-12 columns">
-    <h1>Body Content</h1>
-  </div>
-  <hr>
-</div>
-<div class="row rendered">
-  <div class="large-12 columns">
-    <h5>Rendered HTML</h5>
-  </div>
-</div>
-<?php include 'components/body-content.php';?>
-<div class="row code">
-    <div class="large-12 columns content">
-      <h5>HTML &amp; CSS</h5>
-    </div>
-    <div class="large-12 columns">
-      <h3>Image with text</h3>
-      <script src="https://gist.github.com/capitalJT/b24f560aea1e4799bc59.js"></script>
-      <h3>Sidebar widget</h3>
-      <script src="https://gist.github.com/capitalJT/1195dadb67111da6aa57.js"></script>
-    </div>
-</div>
+<?php include 'includes/lists.php';?>
 
 <? include 'footer.php';?>
