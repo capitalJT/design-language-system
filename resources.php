0d211344fa375668616ae4e13c1d402eeb01e1ec
Jabal Torres <jabaltorres@gmail.com>
2015-03-15 18:38:17 +0300

    shifted around files

--- resources.php
+++ resources.php
@@ -8,9 +8,12 @@
   <div class="large-12 columns toc">
     <h3>Contents</h3>
     <nav>
-      <li><a data-scroll href="#link1">Link 1</a></li>
-      <li><a data-scroll href="#link2">Link 2</a></li>
-      <li><a data-scroll href="#link3">Link 3</a></li>
+      <li><a data-scroll href="#logos">Logos</a></li>
+      <li><a data-scroll href="#style-guide">Style Guide</a></li>
+      <li><a data-scroll href="#typefaces">Typefaces</a></li>
+      <li><a data-scroll href="#icon-font">Icon Font</a></li>
+      <li><a data-scroll href="#zurb-foundation">Zurb Foundation</a></li>
+      <li><a data-scroll href="#sass-mixins">SASS Mixins</a></li>
     </nav>
   </div>
 </section>
@@ -20,60 +23,92 @@
     <h1>Resource Center</h1>
   </div>
   <hr>
-  <article class="large-12 columns">
-    <h3>Resource Name</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
+
+  <article id="logos" class="large-12 columns">
     <h3>AJMN Logos</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
+    <p>The AJMN Logo template includes the logos for all Al Jazeera products and divisions.</p>
+    <a class="icon-link" href="downloads/ajmn-logos.zip" target="blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">AJMN Logos</span>
+    </a>
+    <span class="info block">3.5 MB (.ai)</span>
+  </article><!-- end logos -->
+
+  <article id="style-guide" class="large-12 columns">
     <h3>AJMN Style Guide</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
+    <p>We have produced these brand guidelines to help you create effective communications that reflect who we are and what we stand for.</p>
+    <a class="icon-link" href="https://www.dropbox.com/s/un2siz7zw13zt1l/AJMN_Brand_Guidelines_WIP_11.2.pdf?dl=0" target="_blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">AJMN Brand Guidelines</span>
+    </a>
+    <span class="info block">73.1 MB (.pdf)</span>
+  </article><!-- end style-guide -->
+
+  <article id="typefaces" class="large-12 columns">
     <h3>AJMN Typefaces</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
-    <h3>Vecor Icons</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
-    <h3>Interaction Examples</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
+    <p>Al Jazeera Media Network corporate typefaces are Al Jazeera Arabic and Helvetica Neue LT Pro. These typefaces should be used for all application.</p>
+    <p>Note: All design files within this resource page are dependent on these typefaces.</p>
+    <a class="icon-link" href="downloads/Al-Jazeera-Arabic.zip" target="_blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">Al Jazeera Arabic</span>
+    </a>
+    <span class="info block">112 KB (.zip)</span>
+
+
+    <a class="icon-link" href="downloads/Helvetica-Neue-LT-Pro.zip" target="_blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">Helvetica Neue LT Pro</span>
+    </a>
+    <span class="info block">127 KB (.zip)</span>
+  </article><!-- end typefaces -->
+
+  <article id="icon-font" class="large-12 columns">
+    <h3>Vector Icon Font</h3>
+    <p>Our Icon fonts are incredibly light weight. Because it’s a single font file instead of multiple images, it requires just one HTTP request, which is great for site performance. They can be scaled smoothly to any size, and styled easily using CSS.</p>
+    <a class="icon-link" href="http://github.com/capitalJT/AJMN-font/" target="_blank">
+      <span class="icon ajmn-info"></span>
+      <span class="anchor-text">AJMN Icon Font</span>
+    </a>
+    <span class="info block">GitHub Repository</span>
+
+    <a class="icon-link" href="http://capital-j.com/ajmn/ajmn-icon-font/ajmn-icon-font-ai.zip" target="_blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">AJMN Vector Icons</span>
+    </a>
+    <span class="info block">659 KB (.ai)</span>
+
+    <a class="icon-link" href="http://capital-j.com/ajmn/ajmn-icon-font/ajmn-icon-font-sketch.zip" target="_blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">AJMN Vector Icons</span>
+    </a>
+    <span class="info block">219 KB (.sketch)</span>
+
+    <a class="icon-link" href="http://capital-j.com/ajmn/ajmn-icon-font/ajmn-icon-font-svg.zip" target="_blank">
+      <span class="icon ajmn-download"></span>
+      <span class="anchor-text">AJMN Vector Icons</span>
+    </a>
+    <span class="info block">219 KB (svg)</span>
+  </article><!-- end icon-font -->
+
+  <article id="zurb-foundation" class="large-12 columns">
     <h3>Zurb Foundation</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
-    <h3>SASS Mixins</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
-  <article class="large-12 columns">
-    <h3>Wireframe Templates</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe dolorem beatae, non ullam consectetur voluptate?</p>
-    <a class="download-link" href="#">Link to resource</a>
-    <span class="info block">This is the file size</span>
-  </article>
+    <p>Our projects are built with Zurb Foundation. Foundation is an easy to use, powerful, and flexible framework for building prototypes and production code on any kind of device.</p>
+    <a class="icon-link" href="http://foundation.zurb.com/docs/" target="_blank">
+      <span class="icon ajmn-info"></span>
+      <span class="anchor-text">Foundation Documentation</span>
+    </a>
+    <span class="info block">External link</span>
+  </article><!-- end zurb-foundation -->
+
+  <article id="sass-mixins" class="large-12 columns">
+    <h3>SASS</h3>
+    <p>SASS is short for Syntactically Awesome Style Sheets. It is the most mature, stable, and powerful professional grade CSS extension language in the world.</p>
+    <a class="icon-link" href="http://sass-lang.com/guide" target="_blank">
+      <span class="icon ajmn-info"></span>
+      <span class="anchor-text">SASS Guide</span>
+    </a>
+    <span class="info block">External Link</span>
+  </article><!-- end sass-mixins -->
 
 </section>
 
