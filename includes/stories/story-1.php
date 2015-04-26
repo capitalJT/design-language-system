09506a93ddcf361afc8035ad15c164b4b5fca90d
Jabal Torres <jabaltorres@gmail.com>
2015-04-26 13:29:05 +0300

    connection test

--- includes/stories/story-1.php
+++ includes/stories/story-1.php
@@ -1,7 +1,7 @@
 <article class="story">
 	<a href="#"><img src="http://placehold.it/640x360&text=16:9" alt="Image Title"></a>
 	<a href="#"><h3 class="heading-story">Macedonia: Behind the Facade</h3></a>
-	<p>People & Power investigates whether rising ethnic tensions in Macedonia could result in civil conflict.</p>
+	<p>People &amp; Power investigates whether rising ethnic tensions in Macedonia could result in civil conflict.</p>
 	<p class="meta">
 		<a href="#">Politics</a>,
 		<a href="#">Macedonia</a>,
